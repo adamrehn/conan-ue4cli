b47167e027cf8dda0137ef3eccfb61942ed46241
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-11-21 14:03:32 +1000

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='conan-ue4cli',
-	version='0.0.1',
+	version='0.0.2',
 	description='ue4cli plugin providing Conan wrapper package functionality',
 	classifiers=[
 		'License :: OSI Approved :: MIT License',
