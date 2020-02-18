96b9264cbda3776ebb1977031616724997ba77dc
Adam Rehn <adam.rehn@my.jcu.edu.au>
2020-02-18 10:59:55 +1000

    Factor out Conan configuration functionality for easy reuse

---
+++ conan_ue4cli/ConanTools.py
@@ -0,0 +1,43 @@
+from conans import tools
+import inspect
+
+
+# A dummy config type to pass to Conan
+class DummyConfig(object):
+	def __getattribute__(self, attr):
+		return None
+
+
+class ConanTools(object):
+	'''
+	Provides access to Conan utility functionality whilst ensuring all required configuration is performed
+	'''
+	
+	# Keep track of whether we have already configured Conan
+	_isConanConfigured = False
+	
+	@staticmethod
+	def get(*args, **kwargs):
+		'''
+		Wraps `conans.tools.get()`
+		'''
+		ConanTools._configureConan()
+		return tools.get(*args, **kwargs)
+	
+	@staticmethod
+	def _configureConan():
+		'''
+		Ensures Conan is configured correctly so we can use its utility functionality from outside recipes
+		'''
+		
+		# We only need to perform configuration once
+		if ConanTools._isConanConfigured == True:
+			return
+		
+		# Ensure Conan's global configuration object is not `None` when using Conan 1.22.0 or newer
+		if hasattr(tools, 'get_global_instances') and hasattr(tools, 'set_global_instances'):
+			if 'config' in inspect.signature(tools.set_global_instances).parameters:
+				instances = tools.get_global_instances()
+				tools.set_global_instances(the_output=instances[0], the_requester=instances[1], config=DummyConfig())
+		
+		ConanTools._isConanConfigured = True
--- conan_ue4cli/RecipeCache.py
+++ conan_ue4cli/RecipeCache.py
@@ -1,19 +1,12 @@
 from .PluginConfiguration import PluginConfiguration
-from conans import tools
-import inspect, os, shutil
+from .ConanTools import ConanTools
+import os, shutil
 
 # The URL from which we retrieve the zip file containing the latest recipe data
 RECIPE_ZIP_URL = 'https://github.com/adamrehn/ue4-conan-recipes/archive/master.zip'
 
 # The root directory name of the files in the recipe data zip file
 ZIP_ROOT_DIR = 'ue4-conan-recipes-master'
-
-
-# A dummy config type to pass to Conan
-class DummyConfig(object):
-	def __getattribute__(self, attr):
-		return None
-
 
 class RecipeCache(object):
 	'''
@@ -33,12 +26,6 @@ class RecipeCache(object):
 		Updates the contents of the recipe cache with the latest recipes from our repo
 		'''
 		
-		# Ensure Conan's global configuration object is not `None` when using Conan 1.22.0 or newer
-		if hasattr(tools, 'get_global_instances') and hasattr(tools, 'set_global_instances'):
-			if 'config' in inspect.signature(tools.set_global_instances).parameters:
-				instances = tools.get_global_instances()
-				tools.set_global_instances(the_output=instances[0], the_requester=instances[1], config=DummyConfig())
-		
 		# Remove the cache directory if it already exists
 		cacheDir = RecipeCache.getCacheDirectory()
 		if os.path.exists(cacheDir):
@@ -46,5 +33,5 @@ class RecipeCache(object):
 		
 		# Download and extract the latest recipes from our GitHub repository
 		parentDir = os.path.dirname(cacheDir)
-		tools.get(RECIPE_ZIP_URL, destination=parentDir)
+		ConanTools.get(RECIPE_ZIP_URL, destination=parentDir)
 		shutil.move(os.path.join(parentDir, ZIP_ROOT_DIR), cacheDir)
