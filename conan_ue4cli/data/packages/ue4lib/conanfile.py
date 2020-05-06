990b29f63133db006ddb0e9c710f3d6ecd873214
Martin Wudenka <Martin.Wudenka@gmx.de>
2020-05-07 01:47:57 +0200

    Fix/missing conan author (#8)
    * fix: Add attributes to conan files to be attribute_checker compliant
    
    * fix: Add url attribute to conan files
    
    * feat: Add attributes to wrapper template conan file
    
    Co-authored-by: Martin Wudenka <martin.wudenka@dlr.de>

--- conan_ue4cli/data/packages/ue4lib/conanfile.py
+++ conan_ue4cli/data/packages/ue4lib/conanfile.py
@@ -3,6 +3,17 @@ from conans import ConanFile
 class UE4LibConan(ConanFile):
     name = "ue4lib"
     version = "ue4"
+    description = """The conan-ue4cli Python package is a 
+                    plugin for ue4cli that provides functionality 
+                    for generating and using Conan packages that 
+                    wrap the third-party libraries bundled in the 
+                    Engine/Source/ThirdParty subdirectory of the 
+                    Unreal Engine 4 source tree."""
+    homepage = "https://github.com/adamrehn/conan-ue4cli"
+    author = "Adam Rehn (adam@adamrehn.com)"
+    license = "MIT"
+    url = "https://github.com/adamrehn/conan-ue4cli/tree/master/conan_ue4cli/data/packages/ue4lib"
+
     exports = "*.py"
     build_policy = "missing"
     
--- conan_ue4cli/data/packages/ue4util/conanfile.py
+++ conan_ue4cli/data/packages/ue4util/conanfile.py
@@ -3,6 +3,11 @@ from conans import ConanFile
 class UE4UtilConan(ConanFile):
     name = "ue4util"
     version = "ue4"
+    homepage = "https://github.com/adamrehn/conan-ue4cli"
+    author = "Adam Rehn (adam@adamrehn.com)"
+    license = "MIT"
+    url = "https://github.com/adamrehn/conan-ue4cli/tree/master/conan_ue4cli/data/packages/ue4util"
+
     exports = "*.py"
     build_policy = "missing"
     
