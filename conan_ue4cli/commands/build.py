2101362be4424442a22c8f286f7aab3b363bfb7b
Adam Rehn <adam@adamrehn.com>
2020-05-13 18:08:33 +1000

    Add more fine-grained control of build command behaviour

--- conan_ue4cli/commands/build.py
+++ conan_ue4cli/commands/build.py
@@ -15,7 +15,10 @@ def build(manager, argv):
 	)
 	parser.add_argument('--rebuild', action='store_true', help='Rebuild packages that already have binaries in the local Conan cache if their recipes have changed')
 	parser.add_argument('--dry-run', action='store_true', help='Print Conan commands instead of running them')
-	parser.add_argument('--export-only', action='store_true', help='Export package recipes to the local Conan cache but do not build binaries')
+	parser.add_argument('--no-export', action='store_true', help='Do not export package recipes to the local Conan cache')
+	parser.add_argument('--no-build', action='store_true', help='Do not build or upload binaries for packages')
+	parser.add_argument('--no-cache', action='store_true', help='Do not include recipes from the conan-ue4cli recipe cache when exporting package recipes to the local Conan cache')
+	parser.add_argument('--no-cwd', action='store_true', help='Do not include recipes from the current working directory when exporting package recipes to the local Conan cache')
 	parser.add_argument('-s', '-source', action='append', dest='sources', metavar='DIR', help='Add the specified directory as an additional source of buildable package recipes (the only sources available by default are the conan-ue4cli recipe cache and the current working directory)')
 	parser.add_argument('-o', '-option', action='append', dest='options', metavar='PKG:OPTION=VALUE', help='Specify options to pass to package recipes when building them')
 	parser.add_argument('-user', default=DEFAULT_USER, help='Set the user for the built packages (default user is "{}")'.format(DEFAULT_USER))
@@ -41,45 +44,68 @@ def build(manager, argv):
 	# Retrieve the Unreal Engine version string for the specified Conan profile and use it as the channel
 	channel = ProfileManagement.profileEngineVersion(args.profile)
 	
-	# If the recipe cache directory does not exist (usually because this is our first build), then populate it
-	cacheDir = RecipeCache.getCacheDirectory()
-	if exists(cacheDir) == False:
-		update(manager, argv)
-	
 	# Create a CommandExecutor to run commands or print them, depending on whether we are in dry-run mode
 	executor = CommandExecutor(args.dry_run)
 	
 	# Create our package builder
 	builder = PackageBuilder(args.user, channel, args.profile, args.rebuild, executor)
 	
-	# Iterate over our source directories and export all detected recipes to Conan's local cache
-	# (Keep track of the list of exported package names in case the user asked us to build all of them)
+	# Keep track of the list of exported package names in case the user asked us to build all available packages
 	exported = []
-	sources = [cacheDir, os.getcwd()] + (args.sources if args.sources is not None else [])
-	for source in sources:
-		for recipe in RecipeManagement.listRecipesInDir(source):
-			
-			# Print progress output
-			name, version = recipe
-			print('Exporting recipe for package "{}/{}@{}/{}"...'.format(name, version, args.user, channel), flush=True)
-			
-			# Attempt to export the recipe
-			builder.export(source, name, version)
-			exported.append(name)
 	
-	# If we are only exporting without building binaries then stop here
-	if args.export_only == True:
-		print('Exports completed, skipping build phase.', flush=True)
+	# Determine if we are performing the export step
+	if args.no_export == True:
+		print('Skipping package recipe export step.')
+	else:
+		
+		# If the recipe cache directory does not exist (usually because this is our first build) and we are including it in our export, then populate it
+		cacheDir = RecipeCache.getCacheDirectory()
+		if args.no_cache == False and exists(cacheDir) == False:
+			update(manager, argv)
+		
+		# Gather the list of enabled recipe source directories
+		sources = []
+		if args.no_cache == False:
+			sources.append(cacheDir)
+		if args.no_cwd == False:
+			sources.append(os.getcwd())
+		if args.sources is not None:
+			sources.extend(args.sources)
+		
+		# Report the list of recipe source directories to the user
+		print('Exporting package recipes from the following source directories:')
+		for source in sources:
+			print('- {}'.format(source))
+		print('', flush=True)
+		
+		# Iterate over our source directories and export all detected recipes to Conan's local cache
+		for source in sources:
+			for recipe in RecipeManagement.listRecipesInDir(source):
+				
+				# Print progress output
+				name, version = recipe
+				print('Exporting recipe for package "{}/{}@{}/{}"...'.format(name, version, args.user, channel), flush=True)
+				
+				# Attempt to export the recipe
+				builder.export(source, name, version)
+				exported.append(name)
+		
+	# Determine if we are performing the build and upload steps
+	if args.no_build == True:
+		print('Skipping package build and upload steps.')
 		return
 	
 	# Process the specified list of packages, resolving versions as needed
 	packages = []
 	for arg in args.package:
 		if arg.lower() == 'all':
-			packages.extend(list([
-				(name, RecipeManagement.getLatestVersion(name, args.user, channel))
-				for name in set(exported)
-			]))
+			if len(exported) == 0:
+				raise RuntimeError('the "all" keyword cannot be used when skipping the package recipe export step')
+			else:
+				packages.extend(list([
+					(name, RecipeManagement.getLatestVersion(name, args.user, channel))
+					for name in set(exported)
+				]))
 		elif '==' in arg:
 			packages.append(tuple(arg.split('==', 1)))
 		else:
