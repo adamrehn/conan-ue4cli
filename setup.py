f3faa571a2cd9223d9e6e7a6596132423562c9cf
Adam Rehn <adam@adamrehn.com>
2020-05-14 15:05:20 +1000

    Use README as long description for PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from os.path import abspath, dirname, join
 from setuptools import setup
 
+# Read the README markdown data from README.md
+with open(abspath(join(dirname(__file__), 'README.md')), 'rb') as readmeFile:
+	__readme__ = readmeFile.read().decode('utf-8')
+
 # Read the version number from version.py
 with open(abspath(join(dirname(__file__), 'conan_ue4cli', 'version.py'))) as versionFile:
 	__version__ = versionFile.read().strip().replace('__version__ = ', '').replace("'", '')
@@ -9,6 +13,8 @@ setup(
 	name='conan-ue4cli',
 	version=__version__,
 	description='Integrate third-party libraries into the Unreal Engine with Conan',
+	long_description=__readme__,
+	long_description_content_type='text/markdown',
 	classifiers=[
 		'License :: OSI Approved :: MIT License',
 		'Programming Language :: Python :: 3.5',
