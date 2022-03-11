1b92fc0f736584b665081872e5c4f235c4838640
Jon Colverson <github@vcxz.co.uk>
2022-03-11 05:50:20 +0000

    Fix finding libc++ for UE5 builds

--- conan_ue4cli/data/packages/toolchain-wrapper/conanfile.py
+++ conan_ue4cli/data/packages/toolchain-wrapper/conanfile.py
@@ -35,7 +35,7 @@ class ToolchainWrapper(ConanFile):
         '''
         Attempts to locate the libc++ static library for the specified architecture under the supplied root directory
         '''
-        libraries = glob.glob(join(root, "lib", "Linux", "*{}*".format(architecture), "libc++.a"))
+        libraries = glob.glob(join(root, "lib", "*", "*{}*".format(architecture), "libc++.a"))
         if len(libraries) > 0:
             return libraries[0]
         else:
